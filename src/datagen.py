051dacec7bd7a7cee93d9a23243b8f2318036426
Junaid Muzaffar <62337545+junaid340@users.noreply.github.com>
2022-12-15 10:31:27 +0500

    Add files via upload

---
+++ src/datagen.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:12:34 2022
+
+@author: Junaid
+"""
+
+import os
+from glob import glob
+import cv2
+import numpy as np
+from tqdm import tqdm
+import torch
+from torchvision import transforms
+import random
+
+def read_save_data(path, size, save=True):
+    dir_path = os.listdir(path)  
+    data = {'A':[], 'B':[]}
+    for d in dir_path:
+        category = os.listdir(os.path.join(path, d))    
+        for c in category:
+            imgs_path = os.path.join(path, d, c)
+            imgs_lst = glob(os.path.join(imgs_path, '*.jpg'))
+            for img_path in tqdm(imgs_lst):
+                img = cv2.imread(img_path)
+                img = np.array(cv2.resize(img, size))
+                data[c].append(img)
+    fname = path+'.npz'
+    if save:
+        np.savez_compressed(fname, np.array(data['A']), np.array(data['B']))
+    return data
+    
+
+def load_data(fname):
+    data = np.load(fname)
+    train_A, train_B = data['arr_0'], data['arr_1']
+    return train_A, train_B
+    
+
+class ImageLoader(torch.utils.data.Dataset):
+  def __init__(self, train_data, n_samples, transform):
+        'Initialization'
+        self.train_data = train_data
+        self.n_samples = n_samples
+        self.transform = transform
+        self.total_samples = len(train_data)
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.train_data)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # choose random instances
+        idx = np.random.randint(0, self.total_samples, self.n_samples)      
+    	# retrieve selected images
+        X = self.train_data[idx][0]
+        X = self.transform(X)
+        return X
+    
+
+class ImagePool:
+    """
+        Image pool of 50 generated images for each discriminator model that is 
+        first populated and probabilistically either adds new images to the 
+        pool by replacing an existing image or uses a generated image directly.
+    """
+    def __init__(self, max_size=50):
+        assert (max_size > 0)
+        self.max_size = max_size
+        self.data = []
+
+    def update_image_pool(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return torch.cat(to_return)
+    
+def get_data_loaders(data, batch_size, image_size):
+    train_A, train_B = data
+    transform = transforms.Compose([transforms.ToTensor(), 
+                                    transforms.RandomCrop(image_size),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), 
+                                                         (0.5, 0.5, 0.5))])
+    dataset_A = ImageLoader(train_A, n_samples=batch_size, transform=transform)
+    dataset_B = ImageLoader(train_B, n_samples=batch_size, transform=transform)
+    #train_data, n_samples, patch_shape, transform
+    dataloader_A = torch.utils.data.DataLoader(dataset_A, batch_size)
+    dataloader_B = torch.utils.data.DataLoader(dataset_B, batch_size)
+    return dataloader_A, dataloader_B
+
+def get_test_loaders(test_path, batch_size, image_size):
+    data = read_save_data(test_path, image_size, save=False)
+    a, b = np.array(data['A']), np.array(data['B'])
+    limit = min(len(a), len(b)) 
+    try:
+        a = a[0:limit]
+    except IndexError:
+        b = b[0:limit]
+    testloader_A, testloader_B = get_data_loaders((a, b), batch_size, 
+                                                  image_size)
+    return testloader_A, testloader_B
+
+
+
+if __name__ == '__main__':        
+    data = read_save_data(path='../dataset/data/horse2zebra', size=(256, 256))
---
+++ src/utils.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 18:00:01 2022
+
+@author: Junaid
+"""
+
+import pickle
+import sys
+import os
+import torch
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import torch.nn.parallel
+import numpy as np
+import torchvision.utils as vutils
+from tqdm import tqdm
+from glob import glob
+from nn import get_models
+
+
+def combine_histories(hist_path):
+    all_hists = glob(os.path.join(hist_path, '*.pkl'))
+    history = {}
+    for i, h in tqdm(enumerate(all_hists)):
+        hist = read_pickle(h)
+        if i==0:
+            for k in hist.keys():
+                history[k] = []
+        for key, values in hist.items():
+            history[key].extend(values)
+    write_pickle(history, os.path.join(hist_path, 'history.pkl'))
+
+def get_device(cuda=True):
+    cuda = cuda and torch.cuda.is_available()
+    print("PyTorch version: {}".format(torch.__version__))
+    if cuda:
+        print("CUDA version: {}\n".format(torch.version.cuda))
+    seed = np.random.randint(1, 10000)
+    print("Random Seed: ", seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
+    cudnn.benchmark = True
+    device = torch.device("cuda:0" if cuda else "cpu")
+    print('Device: ', device)
+    return device 
+
+
+def write_pickle(obj, path):
+    with open(path, 'wb') as file:
+        pickle.dump(obj, file)
+   
+        
+def read_pickle(path):
+    with open(path, 'rb') as file:
+        obj = pickle.load(file)
+    return obj
+
+
+def save_network(net, path):
+    torch.save(net.state_dict(), path)
+    
+    
+def load_network(path, net):
+    net.load_state_dict(torch.load(path))
+    return net
+
+
+def get_info_string(idx, history):
+    info = ('Generator Loss: {0:.4f} | '.format(history['gen_loss'][idx])
+            + 'Discriminator Loss: {0:.4f} | '.format(history['disc_loss'][idx])
+            + 'Identity Loss: {0:.4f} | '.format(history['gen_id_loss'][idx])
+            + 'Adversarial Loss: {0:.4f} | '.format(history['gen_adv_loss'][idx])
+            + 'Cycle Loss: {0:.4f} | '.format(history['gen_cycle_loss'][idx])
+            + 'Discriminator Loss A: {0:.4f} | '.format(
+                history['disc_loss_A'][idx])
+            + 'Discriminator Loss B: {0:.4f} | '.format(
+                history['disc_loss_B'][idx])
+            )
+    return info
+
+
+def update_epoch_stats(idx, epoch_history, history, time_taken):
+    #updating history
+    history['gen_loss'].append(np.mean(epoch_history['gen_loss']))
+    history['disc_loss'].append(np.mean(epoch_history['disc_loss']))
+    history['gen_id_loss'].append(np.mean(epoch_history['gen_id_loss']))
+    history['gen_adv_loss'].append(np.mean(epoch_history['gen_adv_loss']))
+    history['gen_cycle_loss'].append(np.mean(epoch_history['gen_cycle_loss']))
+    history['disc_loss_A'].append(np.mean(epoch_history['disc_loss_A']))
+    history['disc_loss_B'].append(np.mean(epoch_history['disc_loss_B']))
+    #time str
+    time_str = f' - Time Taken by epoch: {time_taken}.\n'
+    #getting complete epoch info
+    info = get_info_string(idx, history)
+    print('\nEpoch Completed.' + time_str + info)
+    return history    
+
+def print_inline(string):
+    sys.stdout.write('\r'+string)
+    sys.stdout.flush() 
+    
+    
+def save_models(models, path, idx):
+    ckpt_path = os.path.join(path, 'ckpt', str(idx))
+    os.makedirs(ckpt_path, exist_ok=True)
+    print('\nSaving Models\n')
+    for model in tqdm(models):
+        #save_network()
+        model_path = os.path.join(ckpt_path, model.name+'.pth')
+        save_network(model, model_path)
+        
+        
+def save_images(real_imgs, generators, path, idx):
+    img_path = os.path.join(path, 'images', str(idx))
+    os.makedirs(img_path, exist_ok=True)
+    #decoupling images
+    real_img_A, real_img_B = real_imgs
+    #decoupling generators
+    #generator models
+    generator_B2A, generator_A2B = generators
+    #saving real images
+    vutils.save_image(real_img_A, os.path.join(img_path, 'A_real_img.png'),
+                      normalize=True)
+    vutils.save_image(real_img_B, os.path.join(img_path, 'B_real_img.png'),
+                      normalize=True)
+
+    fake_img_A = 0.5 * (generator_B2A(real_img_B).detach() + 1.0)
+    fake_img_B = 0.5 * (generator_A2B(real_img_A).detach() + 1.0)
+
+    vutils.save_image(fake_img_A.detach(), 
+                      os.path.join(img_path, 'B_fake_img.png'),
+                      normalize=True)
+    vutils.save_image(fake_img_B.detach(),
+                      os.path.join(img_path, 'A_fake_img.png'),
+                      normalize=True)    
+    print(f'Images Saved at path: {img_path}') 
+    
+
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60   
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+def check_checkpoints(dump_path):
+    idx = 0
+    flag = False
+    ckpt_dir_path = os.path.join(dump_path, 'ckpt')
+    ckpt_dir = list(map(lambda x:int(x), os.listdir(ckpt_dir_path)))
+    #ascending sort and flip to get values in descending order
+    ckpt_dir_flipped = np.flip(np.sort(ckpt_dir))
+    for idx in ckpt_dir_flipped:
+        ckpts_path = glob(os.path.join(ckpt_dir_path, str(idx), '*.pth'))
+        if len(ckpts_path)==4:
+            print(f'\nCheckpoints found at epoch: {idx}, Paths:\n{ckpts_path}')
+            flag = True
+            break
+    return flag, idx
+
+def load_checkpoints(models, dump_path):
+    new_models = list()
+    flag, idx = check_checkpoints(dump_path)
+    if flag:
+        ch = input(f'\nCheckpoints found at epoch {idx}, do you want to resume'
+                   + ' training (Y/n): ')
+        if ch.lower()=='y':
+            start_epoch = idx
+            for model in models:
+                ckpt_path = os.path.join(dump_path, 'ckpt', str(idx), 
+                                         model.name+'.pth')
+                new_models.append(load_network(ckpt_path, model))
+            new_models = tuple(new_models)
+            print(f'Training will resume from epoch: {start_epoch+1}')
+        else:
+            start_epoch = 0
+            new_models = models
+            print(f'Training will start from epoch: {start_epoch+1}')
+    else:
+        start_epoch = 0
+        new_models = models
+        print(f'Training will start from epoch: {start_epoch+1}')
+    return start_epoch, new_models
+
+def load_serving_models(ckpt_path, device):
+    models = get_models(device, discriminators=False)
+    new_models = list()
+    for model in models:
+        model_path = os.path.join(ckpt_path, model.name+'.pth')
+        model = load_network(model_path, model)
+        new_models.append(model.eval())
+    return tuple(new_models)
